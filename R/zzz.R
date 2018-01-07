2888486c63c16c3c73c3f344d02c81eacaa864a7
Peter Solymos <psolymos@gmail.com>
2018-01-07 01:45:46 -0700

    config simplified

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,8 @@
                     fields=c("Version", "Date"))
     packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2]))
     if (is.null(getOption("cure4insect"))) {
-        opts <- list(
-            baseurl = "http://ftp.public.abmi.ca/species.abmi.ca/reports",
-            version = "2017",
-            sender = sprintf("x@\\%s", Sys.info()[4]))
-        try(source(system.file("config/defaults.conf", package=pkgname),
-            local=TRUE))
-        options("cure4insect" = opts)
+        options("cure4insect" = as.list(drop(read.dcf(
+            file=system.file("config/defaults.conf", package=pkgname)))))
     }
     invisible(NULL)
 }
