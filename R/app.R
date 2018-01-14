c32d18c7462b241f4ac314d29c5f6cc321d02366
Peter Solymos <psolymos@gmail.com>
2018-01-14 02:26:58 -0700

    app_test

--- R/app.R
+++ R/app.R
@@ -1,4 +1,12 @@
 ## files that need to be available as part of the package for OpenCPU
 app_read_csv <- function(...) read.csv(...)
 
+app_test <- function(...) {
+    l <- list(...)
+    for (i in seq_along(l))
+        l[[i]] <- paste0(names(l)[i], ": ", l[[i]])
+    rval <- paste(l, collapse="\n\n")
+    .send_email(l$address, mimepart=rval)
+    rval
+}
 
