5f8f0a4f2d38b356c6ebfc8b4d28feb97ab0608a
Peter Solymos <psolymos@gmail.com>
2018-01-18 19:07:26 -0700

    right arg

--- R/core_functions.R
+++ R/core_functions.R
@@ -324,7 +324,7 @@ function(boot=TRUE, path=NULL, version=NULL, level=0.9, cores=NULL)
     }
     fun <- function(z, boot=NULL, path=NULL, version=NULL, level=0.9, .c4is) {
         .calculate_results(.load_species_data(z,
-            boot=boot, path=path, version=version, .c4is=.c4is$taxon),
+            boot=boot, path=path, version=version, taxon=.c4is$taxon),
             level=level, .c4is=.c4is)
     }
     OUT <- pblapply(SPP, fun, boot=boot, path=path, version=version,
