9dd135ba75f336b5904d990b4aaffe514280c3d6
Peter Solymos <psolymos@gmail.com>
2018-03-21 12:53:23 -0600

    spelling is optional

--- tests/tests.R
+++ tests/tests.R
@@ -9,3 +9,17 @@ source(f2)
 
 ## poly level prediction
 ## ... to be added ...
+
+## spelling
+if (FALSE) {
+    library(spelling)
+    library(hunspell)
+    check_spelling <- function(x)
+        sort(unique(unlist(hunspell(readLines(x), format = "html"))))
+
+    spell_check_package("~/repos/cure4insect")
+
+    check_spelling("~/repos/cure4insect/README.md")
+    check_spelling("~/repos/cure4insect/inst/www/index.html")
+    check_spelling("~/repos/cure4insect/inst/doc/example-species-report.Rmd")
+}
