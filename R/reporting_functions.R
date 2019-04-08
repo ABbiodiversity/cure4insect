5ce2d1c078e657ca86bbb6acc634efd395584db0
Peter Solymos <psolymos@gmail.com>
2019-04-08 14:20:44 -0600

    grrr

--- R/reporting_functions.R
+++ R/reporting_functions.R
@@ -117,8 +117,9 @@ function(boot=NULL, path=NULL, version=NULL, level=0.9, cores=NULL)
         if (.Platform$OS.type == "windows") {
             cl <- makeCluster(cores)
             clusterEvalQ(cl, library(cure4insect))
-            opts <- getOption("cure4insect")
-            clusterExport(cl, "opts", envir=parent.env())
+            e <- new.env()
+            e$opts <- getOption("cure4insect")
+            clusterExport(cl, "opts", envir=e)
             clusterEvalQ(cl, set_options(opts))
             on.exit(stopCluster(cl))
         } else {
