0263cc121109f05164cb8d0892263a07f31b0d36
Peter Solymos <psolymos@gmail.com>
2020-01-15 23:45:15 -0700

    linting js

--- inst/site/assets/js/species.js
+++ inst/site/assets/js/species.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
   // data based rendering
   $("#title").html(data[0].display + ' &ndash; ' + data[0].tnice);
-  if (data[0].Keep == true) {
+  if (data[0].Keep === true) {
     $("#tag-si").addClass("is-success");
     var SI = Math.round(data[0].SI_Est);
     if (data[0].SI2_Est > 100) {
-      var arrow = '<span class="icon"><i class="fa fa-arrow-up"></i></span>'
+      var arrow = '<span class="icon"><i class="fa fa-arrow-up"></i></span>';
     } else {
-      var arrow = '<span class="icon"><i class="fa fa-arrow-down"></i></span>'
+      var arrow = '<span class="icon"><i class="fa fa-arrow-down"></i></span>';
     }
     $("#tag-si").html('<span>' + SI + '%</span> ' + arrow);
     if (SI > 90) {
@@ -23,25 +23,25 @@ $(document).ready(function(){
   } else {
     $("#SI").hide();
     $("#results").hide();
-  };
-  if (data[0].model_north == true) {
+  }
+  if (data[0].model_north === true) {
     $("#tag-north").addClass("is-success");
     $("#tag-north").text("Yes");
   } else {
     $("#tag-north").addClass("is-danger");
     $("#tag-north").text("No");
-  };
-  if (data[0].model_south == true) {
+  }
+  if (data[0].model_south === true) {
     $("#tag-south").addClass("is-success");
     $("#tag-south").text("Yes");
   } else {
     $("#tag-south").addClass("is-danger");
     $("#tag-south").text("No");
-  };
+  }
   $("#tag-version").text(data[0].version);
   $("#tag-hf").text(data[0].hf);
   $("#tag-veg").text(data[0].veg);
-  if (data[0].Comments == "") {
+  if (data[0].Comments === "") {
     $("#comments").hide();
   } else {
     $("#message").text(data[0].Comments);
