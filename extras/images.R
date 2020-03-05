64667dae3511fc53ea54a972d733a7b496c7e372
Peter Solymos <psolymos@gmail.com>
2020-03-05 09:36:34 -0700

    adding abundance gif

---
+++ extras/images.R
@@ -0,0 +1,41 @@
+library(raster)
+library(cure4insect)
+library(magrittr)
+library(magick)
+
+load_common_data()
+col <- hcl.colors(125, "lajolla")
+
+spp <- "Ovenbird"
+
+r <- "BlackthroatedGreenWarbler" %>%
+    load_species_data(boot=FALSE) %>%
+    rasterize_results()
+m0 <- max(values(r[["NR"]]), na.rm=TRUE)
+m1 <- max(values(r[["NC"]]), na.rm=TRUE)
+i0 <- which(!is.na(values(r[["NR"]])) & values(r[["NR"]]) == m0)
+i1 <- which(!is.na(values(r[["NC"]])) & values(r[["NC"]]) == m1)
+values(r[["NR"]])[i0] <- max(m0, m1)
+values(r[["NC"]])[i1] <- max(m0, m1)
+
+png("_nc.png")
+op <- par(mfcol=c(1,1), oma=c(0,0,0,0), mar=c(0,0,0,0))
+plot(r[["NC"]], axes=FALSE, legend=TRUE, main="", box=FALSE, col=col)
+par(op)
+dev.off()
+
+png("_nr.png")
+op <- par(mfcol=c(1,1), oma=c(0,0,0,0), mar=c(0,0,0,0))
+plot(r[["NR"]], axes=FALSE, legend=TRUE, main="", box=FALSE, col=col)
+par(op)
+dev.off()
+
+img1 <- image_read("_nr.png")
+img2 <- image_read("_nc.png")
+
+img3 <- image_animate(image_morph(c(img1, img2, img1), 10), 20)
+
+image_write_gif(img3, "abundance.gif")
+unlink("_nr.png")
+unlink("_nc.png")
+
